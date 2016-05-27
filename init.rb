c14696be011153d673bb39f9cb093111561123a1
Antonio Armando Couto Bem Filho <coutoarmando@gmail.com>
2016-05-27 00:11:23 -0300

    Corrige

--- init.rb
+++ init.rb
@@ -20,9 +20,9 @@ require 'redmine'
 # Patches to the Redmine core. Will not work in development mode
 require 'dispatcher'
 require 'attachment_patch'
-Dispatcher.to_prepare do
-  Attachment.send(:include, AttachmentPatch)
-end
+# Dispatcher.to_prepare do
+#   Attachment.send(:include, AttachmentPatch)
+# end
 
 # Hooks
 require_dependency 'ezfaq_layouts_hook'
